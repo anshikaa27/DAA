e068e8ee630e7e58c3f15ae5da6b1e32824c72ac
anshikaa27 <98694433+anshikaa27@users.noreply.github.com>
2022-04-02 21:47:10 +0530

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -37,3 +37,49 @@ int main()
     return 0;
 
 }
+
+
+//lab1.2
+#include<iostream>
+using namespace std;
+int Binary_search(int arr[],int l,int r,int x)
+{
+    while(l<=r)
+    {
+        int m=(l+r)/2;
+        if(arr[m]==x)
+            return m;
+        if(arr[m]<x)
+            l=m+1;
+        else
+            r=m-1;
+
+    }
+    return -1;
+}
+int main()
+{
+    int T;
+    cin>>T;
+    for(int i=0;i<T;i++)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        int val;
+        cin>>val;
+        int m;
+        m=Binary_search(arr,0,n,val);
+        if(m==-1)
+            cout<<"Value not Found"<<endl;
+        else
+            cout<<"Present "<<m+1<<endl;
+    }
+    return 0;
+
+}
+
